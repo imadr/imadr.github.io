60b618ee99acdd5c4fa3137f1ac0f7bcd4133b0e
imadr <42851709+imadr@users.noreply.github.com>
2021-03-09 13:14:14 +0100

    simplify

--- js/light.js
+++ js/light.js
@@ -1,9 +1,11 @@
+var theme_button = document.getElementById("light");
 var theme = localStorage.getItem("theme") == null ? "light" : localStorage.getItem("theme");
+theme_button.innerHTML = theme == "light" ? "Dark Mode" : "Light Mode";
 document.body.setAttribute("data-theme", theme);
 
-document.getElementById("menu-icons").innerHTML += " <i id=\"light\" class=\"fas fa-lightbulb\"></i>";
-document.getElementById("light").onclick = function(){
+theme_button.onclick = function(){
 	theme = (theme == "light") ? "dark" : "light";
 	localStorage.setItem("theme", theme);
 	document.body.setAttribute("data-theme", theme);
+    this.innerHTML = theme == "light" ? "Dark Mode" : "Light Mode";
 }
