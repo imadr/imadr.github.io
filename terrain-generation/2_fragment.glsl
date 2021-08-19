cd93d656b4da96b7d7221d655f8c4999cf0b43bb
imadr <42851709+imadr@users.noreply.github.com>
2021-08-19 19:37:17 +0100

    .

--- terrain-generation/2_fragment.glsl
+++ terrain-generation/2_fragment.glsl
@@ -10,19 +10,6 @@ in vec3 position;
 
 void main(){
     float height = texture(heightmap, position.xz/100.).r;
-    height *= height_multiplier/40.;
-    vec3 color = vec3(0.8, 0.8, 0.8);
-    if(height < 0.1){
-        color = vec3(1, 0.9, 0.65);
-    }
-    else if(height >= 0.1 && height < 0.2){
-        color = vec3(0.6, 1, 0);
-    }
-    else if(height >= 0.2 && height < 0.4){
-        color = vec3(0.4, 1, 0);
-    }
-    else if(height >= 0.4 && height < 0.6){
-        color = vec3(0.1, 1, 0);
-    }
+    vec3 color = mix(vec3(1, 0.9, 0.65), vec3(0.1, 1, 0), max(0., height*height_multiplier-5.));
     frag_color = vec4(color, 1);
 }
