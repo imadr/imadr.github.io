8435a0b637b225331a6ae5dc809e3740cfb07e43
imadr <42851709+imadr@users.noreply.github.com>
2021-08-25 21:29:48 +0100

    .

--- js/light.js
+++ js/light.js
@@ -1,3 +1,10 @@
+let css_file = document.createElement("link");
+css_file.rel = "stylesheet";
+css_file.type = "text/css";
+css_file.href = location.pathname == "/" ? "css/style-theme.css" : "../css/style-theme.css";
+document.head.appendChild(css_file);
+
+document.getElementById("menu").innerHTML += ` <span id="light">Dark Mode</span>`;
 let theme_button = document.getElementById("light");
 let theme = localStorage.getItem("theme") == null ? "light" : localStorage.getItem("theme");
 theme_button.innerHTML = theme == "light" ? "Dark Mode" : "Light Mode";
