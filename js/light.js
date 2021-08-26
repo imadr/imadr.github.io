f2a642b0eecfe9f2b80e532d81e2ae9d8aadd5e9
imadr <42851709+imadr@users.noreply.github.com>
2021-08-26 17:01:49 +0100

    .

--- js/light.js
+++ js/light.js
@@ -10,9 +10,32 @@ let theme = localStorage.getItem("theme") == null ? "light" : localStorage.getIt
 theme_button.innerHTML = theme == "light" ? "Dark Mode" : "Light Mode";
 document.body.setAttribute("data-theme", theme);
 
+let svgs = document.getElementsByTagName("object");
+for(let svg of svgs){
+    (function(){
+        svg.onload = function(){
+            let style = document.createElement("style");
+            style.textContent = `
+[data-theme="light"]{
+    --color-fg-1: black;
+}
+[data-theme="dark"]{
+    --color-fg-1: white;
+}
+svg{
+    color: var(--color-fg-1);
+}`;
+            svg.contentDocument.activeElement.appendChild(style);
+        }
+    })();
+}
+
 theme_button.onclick = function(){
-	theme = (theme == "light") ? "dark" : "light";
-	localStorage.setItem("theme", theme);
-	document.body.setAttribute("data-theme", theme);
+    theme = (theme == "light") ? "dark" : "light";
+    localStorage.setItem("theme", theme);
+    document.body.setAttribute("data-theme", theme);
     this.innerHTML = theme == "light" ? "Dark Mode" : "Light Mode";
+    for(let svg of svgs){
+        svg.contentDocument.activeElement.setAttribute("data-theme", theme);
+    }
 }
