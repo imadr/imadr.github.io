7b3c104e9a5355b99a095107504427746f4e97db
imadr <42851709+imadr@users.noreply.github.com>
2022-03-06 21:58:11 +0100

    .

--- rotations-with-quaternions/main.js
+++ rotations-with-quaternions/main.js
@@ -1,25 +1,3 @@
-let canvas1 = document.getElementById("canvas1");
-let gl1 = canvas1.getContext("webgl2");
-canvas1.width = 530;
-canvas1.height = 280;
-
-let canvas2 = document.getElementById("canvas2");
-let gl2 = canvas2.getContext("webgl2");
-canvas2.height = 280;
-
-window.addEventListener("resize", resize_canvas);
-
-function resize_canvas(){
-    let padding = parseFloat(window.getComputedStyle(document.body).getPropertyValue("padding-left"))
-    let width = parseFloat(window.getComputedStyle(document.body).width);
-    width -= padding*2;
-    width = Math.min(530, width);
-    canvas1.width = width;
-    canvas2.width = width;
-}
-
-resize_canvas();
-
 function cube_vertex_buffer(gl){
     let vertex_buffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
@@ -28,9 +6,6 @@ function cube_vertex_buffer(gl){
 
     return [vertex_buffer, vertices.length/3/2];
 }
-
-let objects_to_draw1 = [];
-let objects_to_draw2 = [];
 
 let start = euler_to_quat([0, -Math.PI-Math.PI/2, 0]);
 let end = euler_to_quat([0, 0, 0]);
@@ -50,9 +25,6 @@ let cube = {
     vertex_count: null,
     vao: null,
 };
-
-objects_to_draw1.push(JSON.parse(JSON.stringify(cube)));
-objects_to_draw2.push(JSON.parse(JSON.stringify(cube)));
 
 let main_camera = {
     fov: 60,
@@ -85,14 +57,13 @@ function draw(gl, camera, objects_to_draw){
     let v = lookat_matrix(camera.position, vec3_add(camera.position, forward), [0, 1, 0]);
 
     for(let i = 0; i < objects_to_draw.length; i++){
-        gl.useProgram(objects_to_draw[i].shader.program);
-
         let m = mat4_identity();
         m = mat4_mat4_mul(translate_3d(objects_to_draw[i].transform.position), m);
         m = mat4_mat4_mul(rotate_3d(objects_to_draw[i].transform.rotation), m);
         m = mat4_mat4_mul(scale_3d(objects_to_draw[i].transform.scale), m);
         let mvp = mat4_identity();
         mvp = mat4_mat4_mul(m, mat4_mat4_mul(v, p));
+        gl.useProgram(objects_to_draw[i].shader.program);
         set_shader_uniform(gl, objects_to_draw[i].shader, "mvp", mvp);
 
         gl.bindVertexArray(objects_to_draw[i].vao);
@@ -134,8 +105,8 @@ function init(gl, objects_to_draw){
         objects_to_draw[i].shader.program = shader_program;
         let n_uniforms = gl.getProgramParameter(shader_program, gl.ACTIVE_UNIFORMS);
 
-        for(let i = 0; i < n_uniforms; i++){
-            let uniform = gl.getActiveUniform(shader_program, i);
+        for(let j = 0; j < n_uniforms; j++){
+            let uniform = gl.getActiveUniform(shader_program, j);
             objects_to_draw[i].shader.uniforms[uniform["name"]] = {
                 type: uniform["type"],
                 location: gl.getUniformLocation(shader_program, uniform["name"])
@@ -161,27 +132,28 @@ function init(gl, objects_to_draw){
     }
 }
 
-function demo(objects_to_draw, id){
-    let slider = document.getElementById("slerp-slider"+id);
-    let t_value = document.getElementById("t-value"+id);
+function demo(i){
+    let slider = document.getElementById("slerp-slider-"+canvas_id[i]);
+    let t_value = document.getElementById("t-value-"+canvas_id[i]);
     t_value.innerHTML = (Math.round(parseFloat(slider.value)*100)/100).toFixed(2);
     slider.addEventListener("input", function(e){
         clearInterval(demo_interval);
-        update_lerp();
+        update_lerp(i);
     });
 
     let demo_interval = null;
     let play = true;
-    let demo_button = document.getElementById("play-demo"+id);
+    let demo_button = document.getElementById("play-demo-"+canvas_id[i]);
 
-    function update_lerp(){
-        if(id == 1){
-            objects_to_draw[0].transform.rotation = bad_quat_slerp(start, end, parseFloat(slider.value));
+    function update_lerp(i){
+        if(canvas_id[i] == "lerp"){
+            objects_to_draw[i][0].transform.rotation = bad_quat_slerp(start, end, parseFloat(slider.value));
         }
-        else if(id == 2){
-            objects_to_draw[0].transform.rotation = quat_slerp(start, end, parseFloat(slider.value));
+        else if(canvas_id[i] == "lerp-fixed"){
+            objects_to_draw[i][0].transform.rotation = quat_slerp(start, end, parseFloat(slider.value));
         }
         t_value.innerHTML = (Math.round(parseFloat(slider.value)*100)/100).toFixed(2);
+        update();
     }
 
     demo_button.onclick = function(){
@@ -189,27 +161,81 @@ function demo(objects_to_draw, id){
         if(play){
             demo_interval = setInterval(function(){
                 slider.value = parseFloat(slider.value)+0.002;
-                update_lerp();
+                update_lerp(i);
             }, 1);
         }
         else{
             clearInterval(demo_interval);
-            update_lerp();
+            update_lerp(i);
         }
         play = !play;
         demo_button.innerHTML = play ? "Play" : "Reset";
     };
 }
 
-demo(objects_to_draw1, 1);
-demo(objects_to_draw2, 2);
-
-function update(){
-    draw(gl1, main_camera, objects_to_draw1);
-    draw(gl2, main_camera, objects_to_draw2);
-    window.requestAnimationFrame(update);
+// quick and dirty
+function input_parse(id){
+    return eval(document.getElementById(id).value.replace("π", "Math.PI"));
 }
 
-init(gl1, objects_to_draw1);
-init(gl2, objects_to_draw2);
-update();
+function demo_mult(i){
+    let rot1 = euler_to_quat([input_parse("rot1-0"), input_parse("rot1-1"), input_parse("rot1-2")]);
+    let rot2 = euler_to_quat([input_parse("rot2-0"), input_parse("rot2-1"), input_parse("rot2-2")]);
+    objects_to_draw[i][0].transform.rotation = quat_mul(quat_mul(rot1, rot2), quat_id());
+    update();
+}
+
+let canvases = [];
+let gl = [];
+let objects_to_draw = [];
+
+let canvas_id = ["mult", "lerp", "lerp-fixed"];
+
+for(let i = 0; i < canvas_id.length; i++){
+    let canvas = document.getElementById("canvas-"+canvas_id[i]);
+    canvas.width = 530;
+    canvas.height = 280;
+    canvases.push(canvas);
+    gl.push(canvas.getContext("webgl2"));
+
+    objects_to_draw.push([JSON.parse(JSON.stringify(cube))]);
+    init(gl[i], objects_to_draw[i]);
+
+    switch(canvas_id[i]){
+        case "lerp":
+        case "lerp-fixed":
+            demo(i);
+            break;
+        case "mult":
+            demo_mult(i);
+    }
+}
+
+for(let j = 1; j <= 2; j++){
+    for(let i = 0; i < 3; i++){
+        (function(i, j){
+            document.getElementById("rot"+j+"-"+i).addEventListener("input", function(){
+                demo_mult(0);
+            });
+        })(i, j);
+    }
+}
+
+function update(){
+    for(let i = 0; i < gl.length; i++){
+        draw(gl[i], main_camera, objects_to_draw[i]);
+    }
+}
+
+function resize_canvas(){
+    let padding = parseFloat(window.getComputedStyle(document.body).getPropertyValue("padding-left"))
+    let width = parseFloat(window.getComputedStyle(document.body).width);
+    width -= padding*2;
+    width = Math.min(530, width);
+    for(let i = 0; i < canvases.length; i++){
+        canvases[i].width = width;
+    }
+    update();
+}
+window.addEventListener("resize", resize_canvas);
+resize_canvas();
